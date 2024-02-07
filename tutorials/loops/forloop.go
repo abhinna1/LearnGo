29da1710fd15d3534fef4ed35f86c054fb2d51df
abhinna1 <manandharabhinna@gmail.com>
2024-02-07 21:13:57 +0545

    forloop tutorial / docstring added.

--- tutorials/loops/forloop.go
+++ tutorials/loops/forloop.go
@@ -1,6 +1,10 @@
 package loops
 
 // Insert all the elements up to the limit in a list.
+//	Params:
+// 		limit (int):
+// 	Returns:
+// 		[]int: list with all the integers appended into.
 func PrintCounter(limit int) []int {
 	var x []int
 
